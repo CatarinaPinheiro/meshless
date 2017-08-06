d2c294746952bb5e8434cb6e619d5b333163f01c
Bruno Vieira Costa <brunopsycon@gmail.com>
2017-08-06 16:54:55 -0300

    meshless function

---
+++ functions.m
@@ -0,0 +1,86 @@
+function drawCircle(x,y,r)
+	t = linspace(0,2*pi,100)'; 
+	circsx = r.*cos(t) + x; 
+	circsy = r.*sin(t) + y; 
+	plot(circsx,circsy); 
+end
+
+function weights =  GaussianWithRadius(dist,r)
+	weights = [];
+	c=100;
+	for rr=r'
+		if norm(dist) <= r
+			exp1 = exp(-(norm(dist)/c)^2);
+			exp2 = exp(-(r/c)^2);
+			weight = ( exp1 - exp2 )/(1-exp2);
+		else
+			weight = 0;
+		end
+
+		weights = [weights ; weight];
+	end
+end
+
+function W = W(data,point,r)
+	W=[];
+	for row = 1:rows(data)
+		dat = data(row,:);
+		leftZeroes = zeros(1,row-1);
+		rightZeroes = zeros(1,size(data) - row);
+		weight = GaussianWithRadius(dat(1:2)-point,r);
+		newRow = [leftZeroes weight rightZeroes];
+		W = [W ; newRow];
+	end
+end
+
+function P = createBase(base,data)
+	P=[];
+	for dat=data'
+		row = [];
+		for b=base'
+			command = ["x=" mat2str(dat(1)) "\ny=" mat2str(dat(2)) "\n" b'];
+			row = [row eval(command)];
+		end
+		P = [P ; row];
+	end
+end
+
+function coefficients = coefficients(data,P, x,r)
+	B = P'*W(data,x,r);
+	A = B*P;
+	if(abs(det(A)) < 10^-6)
+		error("determinante muito pequeno!");
+	else
+		coefficients = inv(A)*B*data(:,3);
+	end
+end
+
+function r = getRadius(data,point,m)
+	r = [];
+	distances = [];
+	for p = point'
+		for dat=data'
+			distances = [distances;norm(point'-dat(1:2))];
+		end
+		distances = sort(distances);
+		r = [r;distances(m)];
+	end
+
+end
+
+function u = u(data,base,point)
+	m = size(base)(1);
+	while true
+		try
+			r = getRadius(data,point,m);
+			P = createBase(base,data);
+			c = coefficients(data,P,point,r);
+			p = createBase(base,point);
+		catch
+			m++
+			continue;
+		end
+		break;
+	end
+	u = p*c;
+end
---
+++ main.m
@@ -0,0 +1,37 @@
+source('functions.m')
+source('data.m')
+
+
+ponto = [3 4];
+raio = getRadius(dados,ponto,6);
+
+u(dados,base,[1 2])
+
+hold on;
+scatter(dados(:,1),dados(:,2),'b');
+scatter(ponto(1),ponto(2),'r');
+drawCircle(ponto(1),ponto(2),raio)
+hold off;
+pause();
+
+
+
+tx = ty = linspace (0, 5, 6)';
+[xx, yy] = meshgrid (tx, ty);
+
+tz=[];
+for x=tx'
+	row = [];
+	for y=ty'
+		z = u(dados,base,[x y]);
+		row = [row z];
+	end
+	tz = [tz ; row];
+end
+
+tz
+scatter3(dados(:,1),dados(:,2),dados(:,3));
+mesh (tx, ty, tz);
+pause();
+
+exit();
